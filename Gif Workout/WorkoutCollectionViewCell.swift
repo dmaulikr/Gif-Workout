5879f80f91d61ed1eaa29d242157d48fb3477ed8
mowerb <mowerb@users.noreply.github.com>
2017-07-20 11:44:46 -0600

    Setting up Collection View

---
+++ Gif Workout/WorkoutCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  WorkoutCollectionViewCell.swift
+//  Gif Workout
+//
+//  Created by Zebra on 7/20/17.
+//  Copyright © 2017 Brady Mower. All rights reserved.
+//
+
+import UIKit
+
+class WorkoutCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var workoutImageBG: UIImageView!
+    @IBOutlet weak var workoutNameLabel: UILabel!
+    
+}
--- WorkoutsViewController.swift
+++ WorkoutsViewController.swift
@@ -8,13 +8,26 @@
 
 import UIKit
 
-class WorkoutsViewController: UIViewController {
+private let reuseIdentifier = "Workout Cell"
 
+class WorkoutsViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+
+    @IBOutlet weak var workoutsCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
     
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! WorkoutCollectionViewCell
+        
+        return cell
+    }
 
     /*
     // MARK: - Navigation
