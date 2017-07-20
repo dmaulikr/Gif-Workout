6c1f5c54fae286e1e920578524213bfa380c40b4
mowerb <mowerb@users.noreply.github.com>
2017-07-20 11:32:45 -0600

    New VC, CollectionView Components

---
+++ WorkoutsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WorkoutsViewController.swift
+//  Gif Workout
+//
+//  Created by Zebra on 7/20/17.
+//  Copyright © 2017 Brady Mower. All rights reserved.
+//
+
+import UIKit
+
+class WorkoutsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
