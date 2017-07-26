d0239008e2a12f8dd70ad9e650e4c4d47b68b159
mowerb <mowerb@users.noreply.github.com>
2017-07-26 15:17:04 -0600

    Change color of all navigation bars
    - White and black

--- Gif Workout/AppDelegate.swift
+++ Gif Workout/AppDelegate.swift
@@ -15,7 +15,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        let navigationBarAppearace = UINavigationBar.appearance()
+        
+        // Color of Navigation Bar background
+        navigationBarAppearace.barTintColor = UIColor.white
+        // Color of Navigation Bar Title
+        navigationBarAppearace.titleTextAttributes = [NSForegroundColorAttributeName:UIColor.black]
+        // Color of Navigation Bar back indicator, button titles, button images
+        navigationBarAppearace.tintColor = UIColor.black
+        
         return true
     }
 
