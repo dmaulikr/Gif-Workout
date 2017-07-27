ede72c61c81e250d2a2c1847b05caf2f7401a508
mowerb <mowerb@users.noreply.github.com>
2017-07-27 15:54:15 -0600

    Add outlets from WorkoutView

---
+++ Gif Workout/WorkoutViewController.swift
@@ -0,0 +1,36 @@
+//
+//  WorkoutViewController.swift
+//  Gif Workout
+//
+//  Created by Zebra on 7/27/17.
+//  Copyright © 2017 Brady Mower. All rights reserved.
+//
+
+import UIKit
+
+class WorkoutViewController: UIViewController {
+    
+    @IBOutlet weak var exerciseTitleLabel: UILabel!
+    @IBOutlet weak var exerciseImageView: UIImageView!
+    @IBOutlet weak var darkeningView: UIView!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var bottomTimerLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
